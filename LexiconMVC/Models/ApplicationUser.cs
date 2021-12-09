d27dd8aa4097cbae2f18bc3f359b7d47e286fff8
richard sandblom <r.sandblom@gmail.com>
2021-12-09 09:13:48 +0100

    Fixed date format in register

--- LexiconMVC/Models/ApplicationUser.cs
+++ LexiconMVC/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,8 @@ namespace LexiconMVCData.Models
         [PersonalData]
         public string LastName { get; set; }
         [PersonalData]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime BirthDate { get; set; }
 
     }
